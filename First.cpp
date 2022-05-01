e7fa66c92ee17e2d51d50847afdb72e482d6bba4
Devendra kumar <devendrapal8682@gmail.com>
2022-05-01 05:51:41 +0530

    initial commit

---
+++ First.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+ 
+using namespace std;
+int main()
+{
+        /* code here */
+        string name;
+    cout<<"Enter your name";
+    cin>>name;
+    cout<<"hi "<< name;
+    cout<<"bye";
+    return 0;
+}       
