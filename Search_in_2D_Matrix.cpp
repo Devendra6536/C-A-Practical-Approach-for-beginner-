dd48061983698c11ea89390b29087d891229b5ec
Devendra kumar <devendrapal8682@gmail.com>
2022-05-03 15:01:10 +0530

    Search in 2d matrix is created

---
+++ Search_in_2D_Matrix.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ #include<vector>
+using namespace std;
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        if(matrix.size()==0){
+            return false;
+        }
+        
+        int n= matrix.size();
+        int m = matrix[0].size();
+        
+        int low = 0;
+        int high = (m*n)-1;
+        
+        while(low<=high){
+            
+        int mid  = (low+(high-low)/2);          
+            
+            if(matrix[mid/m][mid%m] == target)
+            {
+                return true;
+            }
+            
+            if(matrix[mid/m][mid%m] > target)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+            
+        }
+        return false;
+    }
+int main()
+{
+        /* code here */
+vector< vector<int> > matrix {
+    {1,2,3,4},
+    {4,5,6,7},
+    {8,9,10,11}
+};
+ searchMatrix(matrix,6)?cout<<"present":cout<<"not presetn";
+    return 0;
+}
