b2716a5543508539784a73183078a0ae767f6f61
Devendra kumar <devendrapal8682@gmail.com>
2022-07-16 00:37:51 +0530

    .

--- pattern1.cpp
+++ pattern1.cpp
@@ -11,5 +11,28 @@ int main(){
         }
         cout<<endl;
     }
+
+cout<<endl;
+int n = 8;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(j==n-1-i){
+                cout<<" Z ";
+            }
+            else 
+            {
+             if((i==0 && j<n-1) || (i==n-1 && j>0))
+            {
+                cout<<" Z";
+            }
+            else{
+                cout<<"  ";
+            }
+            }
+        }
+        cout<<endl;
+    }
     return 0;
 }
