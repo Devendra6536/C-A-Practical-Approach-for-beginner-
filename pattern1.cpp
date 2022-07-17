5843d2ab2d6186769dfc55997debb4bc323a9dd0
Devendra kumar <devendrapal8682@gmail.com>
2022-07-17 14:41:47 +0530

    .

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,42 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // int i,j;
-    // for(i=0;i<5;i++){
-    //     for(j=0;j<4;j++){
-    //         if(i==j){
-    //             cout<<" ";
-    //         }
-    //         cout<<" *";
-    //     }
-    //     cout<<endl;
-    // }
+   int n; 
+    for(int i=0;i<5;i++){
+        for(int j=0;j<4;j++){
+            if(i==j){
+                cout<<" ";
+            }
+            cout<<" *";
+        }
+        cout<<endl;
+    }
 
-// cout<<endl;
-// int n = 8;
-//     for(int i=0;i<n;i++)
-//     {
-//         for(int j=0;j<n;j++)
-//         {
-//             if(j==n-1-i){
-//                 cout<<" Z ";
-//             }
-//             else 
-//             {
-//              if((i==0 && j<n-1) || (i==n-1 && j>0))
-//             {
-//                 cout<<" Z";
-//             }
-//             else{
-//                 cout<<"  ";
-//             }
-//             }
-//         }
-//         cout<<endl;
-//     }
 
 cout<<endl;
-int n = 5;
+cout<<endl;
+ n = 5;
     for(int i=0;i<n*2-1;i++)
     {
         for(int j=0;j<n*2;j++)
@@ -55,31 +34,76 @@ int n = 5;
         cout<<endl;
     }
 
+cout<<endl;
+cout<<endl;
+n = 5;
+    for(int i=0;i<n*2-1;i++)
+    {
+        for(int j=0;j<n*2;j++)
+        {
+             if(i<=n/2+1 && j<=n/2+1 && i==j){
+                cout<<"*";
+            }
+            else if(j==n*2-2-i){
+                cout<<"*";
+            }
+            else 
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
 
+cout<<endl;
+cout<<endl;
+ n = 8;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(j==n-1-i){
+                cout<<" *";
+            }
+            else 
+            {
+             if((i==0 && j<n-1) || (i==n-1 && j>0))
+            {
+                cout<<" *";
+            }
+            else{
+                cout<<"  ";
+            }
+            }
+        }
+        cout<<endl;
+    }
 
-// int n=8;
-// int index = 1;
-// int right=n*n+1;
+        cout<<endl;
+        cout<<endl;
+ n=4;
+int index = 1;
+int right=n*n+1;
 
-//     for(int i=n;i>0;i--){
+    for(int i=n;i>0;i--){
 
-//         for(int l=n; l>i; l--){
-//             cout<<"  ";
-//         }
+        for(int l=n; l>i; l--){
+            cout<<"  ";
+        }
 
-//         for(int j=1;j<=i;j++){
-//             cout<<index++;
-//             cout<<" ";
-//         }
-//         for(int k = 1;k<=i;k++)
-//         {
-//             cout<<right++;
-//             if(k<i) 
-//                 cout<<" ";
-//         }
-//         right = right-(i-1)*2-1;
-//         cout<<endl;
-//     }
+        for(int j=1;j<=i;j++){
+            cout<<index++;
+            cout<<" ";
+        }
+        for(int k = 1;k<=i;k++)
+        {
+            cout<<right++;
+            if(k<i) 
+                cout<<" ";
+        }
+        right = right-(i-1)*2-1;
+        cout<<endl;
+    }
     
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-else 
-            //     cout<<" ";
+cout<<endl;
