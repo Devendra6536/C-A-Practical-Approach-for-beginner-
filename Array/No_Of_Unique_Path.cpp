19f9f7d8d3534a872fd2da6ef369647398250778
Devendra kumar <devendrapal8682@gmail.com>
2022-07-25 13:30:47 +0530

    length of longest consecutive

--- Array/No_Of_Unique_Path.cpp
+++ Array/No_Of_Unique_Path.cpp
@@ -13,9 +13,17 @@ using namespace std;
         return (int)res;
         
     }
+
+
+int path(int i,int j,int m,int n){
+    if(i==n-1 && j==m-1) return 1;
+    else if(i>=n || j>=m) return 0;
+    else return path(i+1,j,m,n)+path(i,j+1,m,m);
+}
 int main()
 {
         /* code here */
-cout<<uniquePaths(3,2);
+cout<<uniquePaths(3,2)<<endl;
+cout<<path(0,0,3,3)<<endl;
     return 0;
 }
