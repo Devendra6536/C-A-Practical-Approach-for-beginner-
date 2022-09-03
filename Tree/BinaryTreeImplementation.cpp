c5bdb0bc361cf3b763be82e1b3c99778c00b19f9
Devendra kumar <devendrapal8682@gmail.com>
2022-09-03 13:40:05 +0530

    Boundary Traversal of a Binary Tree

--- Tree/BinaryTreeImplementation.cpp
+++ Tree/BinaryTreeImplementation.cpp
@@ -170,6 +170,34 @@ bool GetPath(Node* root,vector<int>&v,int khoj){
     v.pop_back();
     return false;
 }
+
+bool leftBoundaryTraversal(Node* root,vector<int>&ds){
+    if(root == NULL) return false;
+    if(root->left == NULL && root->right==NULL) return true;
+    ds.push_back(root->data);
+    if(leftBoundaryTraversal(root->left,ds)==true || 
+    leftBoundaryTraversal(root->right,ds)==true) return true;
+    return false;  
+
+}
+void LeafNode(Node* root,vector<int>&ds){
+    if(root == NULL) return;
+    LeafNode(root->left,ds);
+    if(root->left == NULL && root->right == NULL) ds.push_back(root->data);
+    LeafNode(root->right,ds);
+}
+bool rightBoundaryTraversal(Node* root,stack<int>&ds){
+    if(root == NULL) return false;
+    if(root->left == NULL && root->right==NULL) return true;
+    ds.push(root->data);
+    if(rightBoundaryTraversal(root->left,ds)==true || 
+    rightBoundaryTraversal(root->right,ds)==true) return true;
+    return false;  
+
+}
+
+
+
 int main()
 {
         /* code here */
@@ -231,5 +259,27 @@ int main()
             cout<<endl;
         }
         else cout<<"The Node is not present that you enter";
+        cout<<endl<<endl;
+
+
+
+
+
+
+// Boundary Traversal Of Tree
+        vector<int>ds;
+        leftBoundaryTraversal(root,ds);
+        LeafNode(root,ds);
+        stack<int>st;
+        rightBoundaryTraversal(root->right,st);
+        while(!st.empty()){
+            ds.push_back(st.top());
+            st.pop();
+        }
+        cout<<"The left Boundary, leaf nodes and the right boundary Nodes are ";
+        for(int i=0;i<ds.size();i++){
+                cout<<ds[i]<<" ";
+            }
+            cout<<endl;
     return 0;
 }
