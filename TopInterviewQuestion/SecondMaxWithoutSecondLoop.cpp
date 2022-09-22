08174167eb0b3fc5eebce07f0ab1ffba91644ba8
Devendra kumar <devendrapal8682@gmail.com>
2022-09-22 18:16:49 +0530

    .

--- TopInterviewQuestion/SecondMaxWithoutSecondLoop.cpp
+++ TopInterviewQuestion/SecondMaxWithoutSecondLoop.cpp
@@ -9,6 +9,8 @@ int SecondMaxElement(int arr[],int n){
         if(arr[i]>max1){
             max2 = max1;
             max1 = arr[i];
+        }else if(arr[i] > max2 && arr[i]!=max1){
+            max2 = arr[i];
         }
     }
     return max2;
@@ -17,8 +19,9 @@ int main()
 {
         /* code here */
 
-    int arr[] = {8,11,5,9,7,12};
+    int arr[] = {8,6,3,0,8,3,4,6,2,5};
     int size = sizeof(arr)/sizeof(arr[0]);
+    string
     cout<<"The second max Element  is "<<SecondMaxElement(arr,size);
     return 0;
 }
