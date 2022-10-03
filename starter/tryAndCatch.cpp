23cace4a1637f562e4b3e916fb21ea18dbb1643e
Devendra kumar <devendrapal8682@gmail.com>
2022-10-03 13:51:10 +0530

    exception handling

---
+++ starter/tryAndCatch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// This function signature is fine by the compiler, but not recommended.
+// Ideally, the function should specify all uncaught exceptions and function
+// signature should be "void fun(int *ptr, int x) throw (int *, int)"
+void fun(int *ptr, int x)
+{
+	if (ptr == NULL)
+		throw ptr;
+	if (x == 0)
+		throw x;
+	/* Some functionality */
+}
+
+int main()
+{
+	try {
+	fun(NULL, 0);
+	}
+	catch(...) {
+		cout << "Caught exception from fun()";
+	}
+	return 0;
+}
