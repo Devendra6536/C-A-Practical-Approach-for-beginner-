9aff27c97a1c3b5392107052c5923e2331225439
Devendra kumar <devendrapal8682@gmail.com>
2022-10-07 21:19:03 +0530

    192.127.162.00

--- BST/IntroductionToBst.cpp
+++ BST/IntroductionToBst.cpp
@@ -15,9 +15,39 @@ public:
         this->right = NULL;
     }
 }
+void insertIntoBST(Node* root,int data){
+
+    // base case
+    if(root == NULL){
+        root = new Node(data);
+        return;
+    }
+    else{
+        // if the data is less than root
+        if(data > root->data) root->right = insertIntoBST(root->right,data);
+
+        // if the data is greater than root
+        else root->left = insertIntoBST(root->left,data);
+    }
+    return root;
+}
+Node* insertData(Node* root){
+
+    int data;
+    cin >> data;
+
+    while(data != -1){
+        insertIntoBST(root,data);
+        cin >> data;
+    }
+}
 int main()
 {
         /* code here */
+        cout<< "Enter the data "<<endl;
+        Node* root;
+        root = insertData(root);
+        printBST(root);
 
     return 0;
 }
