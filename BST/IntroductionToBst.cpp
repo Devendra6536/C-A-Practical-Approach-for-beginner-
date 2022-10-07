adcc0c4ca4b9728f0441c4ea5697ea1a1fe41fbe
Devendra kumar <devendrapal8682@gmail.com>
2022-10-07 21:10:12 +0530

    BST has Started

---
+++ BST/IntroductionToBst.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+}
+int main()
+{
+        /* code here */
+
+    return 0;
+}
