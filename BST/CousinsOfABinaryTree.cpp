1d08ddc6830a68ffe88f6dc6c5ca29b07c71d0ff
Devendra kumar <devendrapal8682@gmail.com>
2022-10-14 23:41:50 +0530

    993. Cousins in Binary Tree

---
+++ BST/CousinsOfABinaryTree.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int xparent,yparent,xdepth,ydepth;
+     void checkCousins(TreeNode* root, int x,int y, int depth,int par_val){
+        if(root==NULL) return;
+        
+        if(root->val == x){
+            xdepth = depth;
+            xparent = par_val;
+            return;
+        }
+         if(root->val == y){
+             ydepth = depth;
+             yparent = par_val;
+             return;
+         }
+        checkCousins(root->left, x, y, depth+1, root->val);
+        checkCousins(root->right, x, y, depth+1, root->val);     
+    }
+    bool isCousins(TreeNode* root, int x, int y) {
+        if(root->val == x ||root->val ==y) return false;
+        
+        checkCousins(root,x,y,0,0);
+        
+        if(xdepth == ydepth && xparent != yparent) return true;
+        else return false;
+    }
+int main()
+{
+        /* code here */
+
+    return 0;
+}
