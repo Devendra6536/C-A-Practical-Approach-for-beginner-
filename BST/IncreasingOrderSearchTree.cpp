1ce315628f797b75ac0a595a572f45914203f8af
Devendra kumar <devendrapal8682@gmail.com>
2022-10-14 17:22:36 +0530

    ..

--- BST/IncreasingOrderSearchTree.cpp
+++ BST/IncreasingOrderSearchTree.cpp
@@ -2,6 +2,8 @@
 #include<bits/stdc++.h>
 #include<vector>
 using namespace std;
+
+// Time Complexity is -> O(N) Space Complexity is O(N)
 void inorder(TreeNode* root,vector<int>&v){
         if(root==NULL) return;
         inorder(root->left,v);
@@ -21,6 +23,24 @@ void inorder(TreeNode* root,vector<int>&v){
         }
         return temp;
     }
+
+// Time Complexity is -> O(N) Space Complexity is O(1)
+void inorder(TreeNode* root,TreeNode* &ans){
+        if(!root) return;
+        
+        inorder(root->left,ans);
+        ans->right = new TreeNode(root->val);
+        ans = ans->right;
+        inorder(root->right,ans);
+    }
+TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* temp;
+        TreeNode* ans = new TreeNode();
+        temp = ans;
+        inorder(root,ans);
+        
+        return temp->right;
+}
 int main()
 {
         /* code here */
