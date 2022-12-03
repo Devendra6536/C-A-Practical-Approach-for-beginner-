b7ee500d9baa8ad9acd08ca9fd968100ccc1ee53
Devendra kumar <devendrapal8682@gmail.com>
2022-12-03 14:37:05 +0530

    .

--- Subarray/subarray1.cpp
+++ Subarray/subarray1.cpp
@@ -28,6 +28,35 @@ int sums=0;
             cout<<endl;
         }
     }
-    cout<<sums<<endl;   
+    cout<<sums<<endl; 
+
+// o(N^2) solution
+    sums=0;
+    for(int i=0;i<n;i++){
+        int currSum=0;
+        vector<int>v;
+        for(int j=i;j<n;j++){
+            v.push_back(arr[j]);
+            currSum += arr[j];
+        }
+        if(currSum == 6) {
+            for(int k=0;k<v.size();k++){
+                cout<<v[k]<<" ";
+            }
+            cout<<endl;
+        }
+            sums = max(currSum,sums);
+    }
+    cout<<sums<<endl; 
+
+
+    int maxSum=INT_MIN;
+    int currSum=0;
+    for(int i=0;i<n;i++){
+        currSum += arr[i];
+        if(currSum> maxSum) maxSum = currSum;
+        if(currSum<0) currSum = 0;
+    }  
+    cout<<maxSum<<endl;
     return 0;
 }       
