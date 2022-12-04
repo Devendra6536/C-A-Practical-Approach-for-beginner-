45b70a81ee37823bf47eb884f7e0d3ee1fc81b13
Devendra kumar <devendrapal8682@gmail.com>
2022-12-04 16:13:49 +0530

    ..

--- Subarray/NoOfSubArray_Xor.cpp
+++ Subarray/NoOfSubArray_Xor.cpp
@@ -16,10 +16,24 @@ int solve(vector<int>&A,int b){
     }
     return c;
 }
+int count(vector<int>v,int b){
+    int cnt=0;
+    int n = v.size();
+    int xxor = 0;
+    map<int,int>mp;
+    for(int i=0;i<n;i++){
+        xxor^=v[i];
+        if(xxor==b) cnt++;
+        if(mp.find(xxor^b) != mp.end()) cnt+=mp[xxor^b];
+        mp[xxor]++;
+    }
+    return cnt;
+}
 int main()
 {
         /* code here */
 vector<int>A{4,2,2,6,4};
     cout<<"the no of subarray with xor "<<solve(A,6);
+    cout<<"the no of subarray with xor "<<count(A,6);
     return 0;
 }
