dc2b477e47d5b3248c4081879254bc3796bf1745
Devendra kumar <devendrapal8682@gmail.com>
2022-12-06 22:27:07 +0530

    c

---
+++ SelfPractice/pointer.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+int main()
+{
+        /* code here */
+        /*int* arr = new int[5];
+        cout<<sizeof(arr)<<endl;
+        int i = 10;
+        cout<<sizeof(i)<<endl;
+        int *ptr = &i;
+        cout<<sizeof(ptr)<<endl;
+        char ch='q';
+        cout<<sizeof(ch)<<endl;
+        char *p = &ch;
+        cout<<sizeof(p)<<endl;
+        
+
+        int *p;
+        cout<<p<<endl;
+        cout<<*p<<endl;
+        cout<<sizeof(p)<<endl;
+        int a;
+        cout<<a<<endl;*/
+
+        int a,b,c;
+        a=b=c=10;
+        cout<<a<<" "<<b<<" "<<c<<endl;
+
+
+
+    return 0;
+}
